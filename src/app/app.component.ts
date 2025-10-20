e6b21474e62c865f937dce7310f505ba477b4fd0
isabella <bellabernalcarvajal871@gmail.com>
2025-10-20 13:04:55 -0500

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'mi-proyecto-angular';
+    selector: 'app-root',
+    imports: [RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+    })
+    export class AppComponent {
+    title = 'mi-proyecto-angular';
 }
