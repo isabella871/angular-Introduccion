d6af35dd0befe396bf93f1eaa4204b48bc8ffe24
isabella <bellabernalcarvajal871@gmail.com>
2025-10-23 09:23:24 -0500

    componente home y todo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TodoComponent } from './components/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+
+    {
+        path: 'Tareas',
+        component: TodoComponent
+    }
+];
