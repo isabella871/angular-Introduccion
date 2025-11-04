83b3d4da54840c0f9e8a81572d98e1530c2d429a
isabella <bellabernalcarvajal871@gmail.com>
2025-11-04 09:05:35 -0500

    Instalación de tailwindcss

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, output } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
@@ -6,7 +6,11 @@ import { Component, Input } from '@angular/core';
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
+
 export class PerfilComponent {
   @Input() userName: string = '';
   @Input() edad: number = 0;
 }
+
+// Evento con @output para enviar datos a HomeComponent
+@Output() edadChange = new EventEmitter<number>(); 
